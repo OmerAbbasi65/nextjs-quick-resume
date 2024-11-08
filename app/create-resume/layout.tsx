3a0c03336c40b9698a0d4d820f5f77a312179e46
Omer Abbasi <abbasiomer65@gmail.com>
2024-11-08 14:14:35 -0800

    Layout fixed

--- app/create-resume/layout.tsx
+++ app/create-resume/layout.tsx
@@ -1,14 +1,15 @@
 // app/create-resume/layout.tsx
 "use client";
 
-import { FormProvider } from '../context/FormContext'; // Adjust the import path as necessary
+import { ReactNode } from "react";
+import { FormProvider } from "../context/FormContext"; // Adjust the import path as necessary
 
-const CreateResumeLayout = ({ children }) => {
-  return (
-    <FormProvider>
-      {children}
-    </FormProvider>
-  );
+interface CreateResumeLayoutProps {
+  children: ReactNode;
+}
+
+const CreateResumeLayout: React.FC<CreateResumeLayoutProps> = ({ children }) => {
+  return <FormProvider>{children}</FormProvider>;
 };
 
 export default CreateResumeLayout;
