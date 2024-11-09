7d7b4db533d3e42933f3ece05929e3c8342dbc96
Omer Abbasi <abbasiomer65@gmail.com>
2024-11-09 05:16:56 -0800

    analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { FormProvider } from "./FormContext";
+import { Analytics } from "@vercel/analytics/react"
 
 
 const geistSans = localFont({
@@ -30,7 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <FormProvider>{children}</FormProvider>
+        <FormProvider>{children} <Analytics/></FormProvider>
       </body>
     </html>
   );
